0eec7dba8a49d32c920d94f417a8fb5e8b936483
enricobellato <enricobellatodevelopment@gmail.com>
2024-03-16 12:02:11 +0000

    Update anthropic

--- tests/anthropic.ts
+++ tests/anthropic.ts
@@ -4,29 +4,27 @@ const anthropic = new Anthropic({
   apiKey: process.env['ANTHROPIC_API_KEY'], // This is the default and can be omitted
 });
 
-const messages = [
-    {
-        role: 'user',
-        content: 'Hello, Claude'
-    },
-    {
-        type: 'assistant',
-        content: "Hello! It's nice to meet you. How can I assist you today?"
-    },
-    {
-        role: 'user',
-        content: 'Who are you?'
-    }
-]
-
 async function main() {
   const message = await anthropic.messages.create({
     max_tokens: 1024,
-    messages: messages,
+    messages: [
+      {
+        role: 'user',
+        content: 'Hello, Claude'
+      },
+      {
+          role: 'assistant',
+          content: "Hello! It's nice to meet you. How can I assist you today?"
+      },
+      {
+          role: 'user',
+          content: 'Who are you?'
+      }
+    ],
     model: 'claude-3-opus-20240229',
   });
 
-  console.log(message.content);
+  console.log(message.content[0].text);
 }
 
 main();
