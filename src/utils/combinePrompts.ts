810b6dc41abb62bd223ce590201e7acfafd61e45
enricobellato <enricobellatodevelopment@gmail.com>
2024-04-19 08:57:18 +0000

    Remove unused function

--- src/utils/combinePrompts.ts
+++
@@ -1,7 +0,0 @@
-import { Message } from '../types';
-
-export function combinePrompts(messages: Message[]): string {
-  return messages.reduce((acc, message) => {
-    return (acc += message.content);
-  }, '');
-}
